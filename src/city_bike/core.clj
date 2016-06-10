a395d5816755c288000d9bafe1a0f90b1ca0e0ba
Lawrence Qiu <lqiu96@gmail.com>
2016-06-10 01:14:43 -0400

    Added function to parse data from bike data csv file.

--- src/city_bike/core.clj
+++ src/city_bike/core.clj
@@ -1,5 +1,20 @@
 (ns city-bike.core
+  (require [clojure.string :as str])
   (:gen-class))
+
+(def header [:duration :start-time :stop-time :start-station-id
+             :start-station-name :start-station-lat :start-station-long
+             :end-station-id :end-station-name :end-station-lat
+             :end-station-long :bike-id :user-type :birth :gender])
+
+(defn parse-csv-data
+  [file-name]
+  (->> (slurp file-name)
+       (str/split-lines)
+       (map #(str/replace % #" " "-"))
+       (map #(str/split % #","))
+       (rest)
+       (map #(zipmap header %))))
 
 (defn -main
   "I don't do a whole lot ... yet."
