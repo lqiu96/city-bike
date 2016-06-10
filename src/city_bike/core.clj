3ce38623be80c886c5d04e845f1018976f71c439
Lawrence Qiu <lqiu96@gmail.com>
2016-06-10 01:15:51 -0400

    Added function to find all *.csv files in current directory.

--- src/city_bike/core.clj
+++ src/city_bike/core.clj
@@ -1,11 +1,17 @@
 (ns city-bike.core
-  (require [clojure.string :as str])
+  (require [clojure.string :as str]
+           [clojure.java.io :as io])
   (:gen-class))
 
 (def header [:duration :start-time :stop-time :start-station-id
              :start-station-name :start-station-lat :start-station-long
              :end-station-id :end-station-name :end-station-lat
              :end-station-long :bike-id :user-type :birth :gender])
+
+(defn get-csv-files
+  []
+  (let [file-vec (vec (file-seq (io/file (System/getProperty "user.dir"))))]
+    (map #(.getName %) (filter #(boolean (re-find #".csv" (.getName %))) file-vec))))
 
 (defn parse-csv-data
   [file-name]
