f46673cfec8491299fdb51768c28f603b896fd8e
Lawrence Qiu <lqiu96@gmail.com>
2016-06-10 01:18:56 -0400

    Updated project version and added comments.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject city-bike "0.1.0-SNAPSHOT"
+(defproject city-bike "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/lqiu96/city-bike"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
--- src/city_bike/core.clj
+++ src/city_bike/core.clj
@@ -9,11 +9,16 @@
              :end-station-long :bike-id :user-type :birth :gender])
 
 (defn get-csv-files
+  "Looks into the current directory's files and filds all
+  files that match are csv file. Puts them into a vector."
   []
   (let [file-vec (vec (file-seq (io/file (System/getProperty "user.dir"))))]
     (map #(.getName %) (filter #(boolean (re-find #".csv" (.getName %))) file-vec))))
 
 (defn parse-csv-data
+  "Reads all the data from csv file and splits the data into a map data structure.
+  First splits based of a new-line then commas. Data is then put into a map with the header
+  keyword being the map key."
   [file-name]
   (->> (slurp file-name)
        (str/split-lines)
@@ -21,6 +26,8 @@
        (map #(str/split % #","))
        (rest)
        (map #(zipmap header %))))
+
+(println (map parse-csv-data (get-csv-files)))
 
 (defn -main
   "I don't do a whole lot ... yet."
